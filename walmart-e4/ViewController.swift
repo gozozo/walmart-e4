597fab38d45c0d3623a3592c4847ab6239707b19
Luis Enrique Vazquez Escobar <luis.vazqueze@elektra.com.mx>
2024-10-09 09:17:13 -0600

    Base project

---
+++ walmart-e4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  walmart-e4
+//
+//  Created by Luis Enrique Vazquez Escobar on 09/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ walmart-e4UITests/walmart_e4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  walmart_e4UITestsLaunchTests.swift
+//  walmart-e4UITests
+//
+//  Created by Luis Enrique Vazquez Escobar on 09/10/24.
+//
+
+import XCTest
+
+final class walmart_e4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
